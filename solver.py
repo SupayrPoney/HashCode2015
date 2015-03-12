dd94c4e7f7ad5eb985f074934b0cea25e72a778c
brochape <brochape@ulb.ac.be>
2015-03-12 19:51:23 +0100

    changed the names of the attributes

--- solver.py
+++ solver.py
@@ -4,20 +4,23 @@ class Solver:
 
     def parse(self, filename):
         with open(filename, "r") as f:
-            self.r, self.s, self.u, self.p, self.m = map(int, f.readline().strip().split())
+            self.rangesNumber, self.slotsPerRange, self.unusedNumber, self.groupsNumber, self.serversNumber = map(int, f.readline().strip().split())
 
-            self.grid = [[0 for i in range(self.s)] for j in range(self.r)]
+            self.grid = [[0 for i in range(self.slotsPerRange)] for j in range(self.rangesNumber)]
 
             # unavailable spots
-            for i in range(self.u):
+            for i in range(self.unusedNumber):
                 r, s = map(int, f.readline().strip().split())
                 self.grid[r][s] = "X"
 
             # all servers
             self.servers = []
-            for i in range(self.r-self.u):
+            for i in range(self.rangesNumber-self.unusedNumber):
                 s, c = map(int, f.readline().strip().split())
                 self.servers.append((s,c))
 
-if __name__ == "__main__":
-    Solver("dc.in")
+
+   
+
+a= Solver("dc.in")
+print a.rangesNumber
