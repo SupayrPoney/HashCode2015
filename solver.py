b656fcbda5949194c97657ff92862e7ac2cf386f
brochape <brochape@ulb.ac.be>
2015-03-12 20:35:10 +0100

    Added the score

--- solver.py
+++ solver.py
@@ -31,5 +31,37 @@ class Solver:
             j+=1
         return freespacesleft
 
+
+    def getServIngroupInRow(self,group,row):
+        res=[]
+        i=0
+
+        while (i<self.slotsPerRange):
+            if self.grid[row][i] == 'x':
+                i+=1
+                continue
+            if self.grid[row][i] == -1:
+                i+=1
+                continue
+            if self.servers[self.grid[row][i]][2] == group:
+                res.append(self.servers[self.grid[row][i]])
+                i+= self.servers[self.grid[row][i]][0]
+
+        return res
+
+    def getGaranteedCapacity(self, groupIndex):
+        res = 0
+        values = []
+        servers = []
+        servers.append(serversInGroup(groupIndex))#servers[i] = servers in the group i
+        totcap = sum(map(lambda x: x[1], servers))
+
+        return totcap - max(sum(map(lambda x : x[1], self.getServIngroupInRow())))
+
+    def getScore(self):
+        return min(map(self.getGaranteedCapacity, range(self.groupsNumber)))
+
+
+
 if __name__ == "__main__":
     Solver("dc.in")
