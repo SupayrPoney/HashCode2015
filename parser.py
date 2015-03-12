d5273a9ec5ef4014c11005348cd01afaa8e4b497
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-03-12 19:07:47 +0100

    [add] parser

---
+++ parser.py
@@ -0,0 +1,23 @@
+class Parser:
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            self.r, self.s, self.u, self.p, self.m = map(int, f.readline().strip().split())
+            print self.r, self.s, self.u, self.p, self.m
+
+            self.grid = [[0 for i in range(self.s)] for j in range(self.r)]
+
+            # unavailable spots
+            for i in range(self.u):
+                r, s = map(int, f.readline().strip().split())
+                self.grid[r][s] = "X"
+
+            # all servers
+            self.servers = []
+            for i in range(self.r-self.u):
+                s, c = map(int, f.readline().strip().split())
+                self.servers.append((s,c))
+
+            for line in self.grid:
+                print line
+
+Parser("dc.in")
