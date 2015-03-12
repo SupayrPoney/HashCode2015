2d98c801bf64b695d7b354806171abc09d5989fd
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-03-12 19:09:21 +0100

    [enh] no prints in parser

--- parser.py
+++ parser.py
@@ -2,7 +2,6 @@ class Parser:
     def __init__(self, filename):
         with open(filename, "r") as f:
             self.r, self.s, self.u, self.p, self.m = map(int, f.readline().strip().split())
-            print self.r, self.s, self.u, self.p, self.m
 
             self.grid = [[0 for i in range(self.s)] for j in range(self.r)]
 
@@ -16,8 +15,5 @@ class Parser:
             for i in range(self.r-self.u):
                 s, c = map(int, f.readline().strip().split())
                 self.servers.append((s,c))
-
-            for line in self.grid:
-                print line
 
 Parser("dc.in")
